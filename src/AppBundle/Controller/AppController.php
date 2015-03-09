e7dd73c33109910512e9f86b128a29270428571b
nathan <nathanyang@lunchactually.com>
2015-03-09 20:58:42 +0800

    add detail

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+
 
 /**
  * Class AppController
@@ -27,6 +29,14 @@ class AppController extends Controller
      */
     public function listAction(){
         return array();
+    }
+
+    /**
+     * @Route("detail/{id}")
+     * @Template()
+     */
+    public function detailAction($id){
+        return new Response(array());
     }
 
 }
