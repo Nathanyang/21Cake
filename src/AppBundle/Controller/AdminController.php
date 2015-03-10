3ec7cf45c51c2d4053af285f793af0de0ef58dda
nathan <nathanyang@lunchactually.com>
2015-03-10 21:12:41 +0800

    del old-admin

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -12,9 +12,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Type;
+
 /**
  * Class AdminController
  * @Route("/admin/")
+ * @Template("")
  */
 class AdminController {
     /**
@@ -31,6 +35,72 @@ class AdminController {
      * @Template()
      */
     public function loginAction(){
+        return array();
+    }
+
+    /**
+     * 添加类型
+     * @Route("type/new", name="add-type")
+     * @Template("AppBundle:Admin:Type/add.html.twig")
+     */
+    public function newTypeAction(Request $request){
+        $type = new Type();
+
+        $form = $this->createFormBuilder($type)
+            ->add('name', 'text')
+            ->add('code', 'text')
+            ->add('save', 'submit', array('label' => 'Create Cake Type'))
+            ->getForm();
+
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * 修改类型
+     * @Route("type/update/{id}", name="update-type")
+     * @Template("AppBundle:Admin:Type/update.html.twig")
+     */
+    public function modTypeAction($id){
+        return array();
+    }
+
+    /**
+     * 类型列表
+     * @Route("type", name="type-list")
+     * @Template("AppBundle:Admin:Type/list.html.twig")
+     */
+    public function typeAction(){
+        return array();
+    }
+
+
+
+    /**
+     * 添加类型
+     * @Route("crowd/add", name="add-crowd")
+     * @Template("AppBundle:Admin:Crowd/add.html.twig")
+     */
+    public function addCrowdAction(){
+        return array();
+    }
+
+    /**
+     * 修改类型
+     * @Route("crowd/update/{id}", name="update-crowd")
+     * @Template("AppBundle:Admin:Crowd/update.html.twig")
+     */
+    public function modCrowdAction($id){
+        return array();
+    }
+
+    /**
+     * 类型列表
+     * @Route("crowd", name="crowd-list")
+     * @Template("AppBundle:Admin:Crowd/list.html.twig")
+     */
+    public function crowdAction(){
         return array();
     }
 
